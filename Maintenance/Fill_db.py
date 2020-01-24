155952c81e7baedd9ad9b959b0a16c00ca3d2048
Semyon <S.Tsutsulenko@sevkab.ru>
2020-01-24 17:22:36 +0300

    Обновил модель БД, добавил методы для новых таблиц

--- Maintenance/Fill_db.py
+++ Maintenance/Fill_db.py
@@ -12,11 +12,13 @@ data = XML_Reader.get(PathToFile / "QASource.xml")
 
 data = sorted(data, key=lambda x: x['group'])
 
-# for i in data:
-#     print(var.add_set(i['group'], i['question'], i['answer']))
-#     DB_Handle.Handler().add_set(i['group'], i['question'], i['answer'])
+for i in data:
+    DB_Handle.Handler().add_set(i['group'], i['question'], i['answer'])
 
-
+DB_Handle.Handler().add_group(1, 'ООП, Паттерны, проектирование')
+DB_Handle.Handler().add_group(2, 'C#')
+DB_Handle.Handler().add_group(3, 'Базовые вопросы по разработке')
+DB_Handle.Handler().add_group(5, 'Python')
 
 
 for x in range(10):
