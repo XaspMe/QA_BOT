de004efe9ef78d656738540dc06fa273c91bb2a1
Semyon <S.Tsutsulenko@sevkab.ru>
2020-01-29 16:57:16 +0300

    Вернул конфиг

---
+++ Core/Configuration.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+s
+class Configuration:
+    """
+    Represent application configuration.
+    TODO: Создать config.ini файл для чтения конфигурации из него.
+    """
+
+    def __init__(self):
+        path_to_core = Path(str(Path().cwd())[:str(Path().cwd()).index('QA_BOT') + 6])
+        self.db_name = path_to_core / 'QA_DB.db'  # Имя базы данных.
+        self.log_name = ""
+        self.log_path = ""
+        self.token = "698296687:AAFQl6Po6wpxBFXH-qHcrlii9BQCxFDkUJk"  # Telebot token
+        self.wan_check_adress = {'8.8.8.8', '09.185.108.134', '209.185.108.135', '209.185.108.138', '209.185.108.139'}
+
+    def init_db_path(self):
+        pass
